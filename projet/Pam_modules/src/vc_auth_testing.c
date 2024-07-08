4c541f1fc7daf22d9d4b58f230b79c35c7847061
Baptiste Marchand <baptiste.marchand01@gmail.com>
2024-07-08 12:00:30 +0200

    Selective Disclosure adapted

--- projet/Pam_modules/src/vc_auth_testing.c
+++ projet/Pam_modules/src/vc_auth_testing.c
@@ -12,13 +12,18 @@
 #include <assert.h>
 #include <curl/curl.h>
 
+#include "../include/logger.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../include/logger.h"
+
+#define TOKEN_FILE ".token"
+#define TOKEN_FIELD "sd_jwt_token"
+
+//#define TRUSTED_CERTIFICATE_PATH "/etc/ssl/certs/keycloak-vc.pem"
+#define TRUSTED_CERTIFICATE_PATH "/etc/ssl/certs/keycloak_verifiable-credentials.pem"
 
 int main() {
-    logger("bonjour", "bonjour");
   char full_sd_jwt[] =
       "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJHd1pmZHVfeEE1TVlKbUlX"
       "bnJBZHZiRXhNTVdHZHNxZlkycW9xRlZoVlBNIn0."
@@ -43,68 +48,30 @@ int main() {
   char *jwt = NULL;
   char **parsed_sd_jwt = NULL;
   unsigned long ndisclosures;
-  parse_SD_JWT_VC(full_sd_jwt, &parsed_sd_jwt, &ndisclosures);
+
+  char *sd_jwt2 = NULL;
+  read_token(".token", &sd_jwt2, "sd_jwt_token");
+  printf("Just read token\n");
+
+  parse_SD_JWT_VC(sd_jwt2, &parsed_sd_jwt, &ndisclosures);
 
   // printf("Voici le jwt : %s \n", jwt);
-  printf("Addresses of the parsed SD-JWT : %p\n", *parsed_sd_jwt);
   printf("Voici les chaines de char du tableau : \n");
   PrintArray(parsed_sd_jwt, ndisclosures);
   // assert(ndisclosures == 4);
 
   printf("First disclosure Decoded : %s \n", base64_decode(parsed_sd_jwt[1]));
 
-  const char *jwttocheck =
-      "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJHd1pmZHVfeEE1TVlKbUlX"
-      "bnJBZHZiRXhNTVdHZHNxZlkycW9xRlZoVlBNIn0."
-      "eyJfc2QiOlsiYUlxX2x3WTdMY1NfNTM1RzhfQVp1U2x0SXBTcW53amFwVjFNd05nQkZicyIs"
-      "ImRDMkljS0R1SS15WUg1S1U3Nnd0aGlURWIyR3FFZ1A3dVllek5Wb241aDAiLCJxaDNGYW5R"
-      "ZlVpMjVOTWFvcGdZcGtKcDB5MlRHZlR3QWJXS3B4VlB4RUZRIl0sIl9zZF9hbGciOiJTSEEt"
-      "MjU2IiwiaXNzIjoiZGlkOndlYjp0ZXN0Lm9yZyIsIm5iZiI6MTcxOTIzNDk1NSwidmN0Ijoi"
-      "TmF0dXJhbFBlcnNvbkNyZWRlbnRpYWwiLCJqdGkiOiJ1cm46dXVpZDo5MWI5YWFhZC1mYTcx"
-      "LTRiNmItODUzYy1hYTQ4ZGYwNDdlMGYifQ."
-      "RWg2UUqluXsnrSDDkwQ0Dz6M1N3fGO9XjBZenEMIAOTbfOQwjG5eU9jDqRlysSaQ6nffBR1I"
-      "G6Gx1HDq_"
-      "e8yu33l192GMf6oyTI3QX7I2D4TdWWLTSNfBbxIs9jpaUMpkBmz01NBjsKFOsaz5X09wHH3C"
-      "gfT3BRMiDd3Qi_iDBaNSZ5pz_ij3TD_nLIsfGikxCpPlIj8KPAIffrWczl4X5ikNh8Bj-"
-      "X67YbDKZ0iHtEhU18vaGHE6YpBCY_Kl6DBGEJ_fYiN33sP5hjuRJkaNCpHYj_"
-      "Ix6cqJUtZeHSIxez1Q4Jq3KMIVi5Mi5fqpai6hoNcG-mjFbm9jLI1cEh8PQ";
-
-  const char *token =
-      "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJHd1pmZHVfeEE1TVlKbUlX"
-      "bnJBZHZiRXhNTVdHZHNxZlkycW9xRlZoVlBNIn0."
-      "eyJfc2QiOlsiYUlxX2x3WTdMY1NfNTM1RzhfQVp1U2x0SXBTcW53amFwVjFNd05nQkZicyIs"
-      "ImRDMkljS0R1SS15WUg1S1U3Nnd0aGlURWIyR3FFZ1A3dVllek5Wb241aDAiLCJxaDNGYW5R"
-      "ZlVpMjVOTWFvcGdZcGtKcDB5MlRHZlR3QWJXS3B4VlB4RUZRIl0sIl9zZF9hbGciOiJTSEEt"
-      "MjU2IiwiaXNzIjoiZGlkOndlYjp0ZXN0Lm9yZyIsIm5iZiI6MTcxOTIzNDk1NSwidmN0Ijoi"
-      "TmF0dXJhbFBlcnNvbkNyZWRlbnRpYWwiLCJqdGkiOiJ1cm46dXVpZDo5MWI5YWFhZC1mYTcx"
-      "LTRiNmItODUzYy1hYTQ4ZGYwNDdlMGYifQ."
-      "RWg2UUqluXsnrSDDkwQ0Dz6M1N3fGO9XjBZenEMIAOTbfOQwjG5eU9jDqRlysSaQ6nffBR1I"
-      "G6Gx1HDq_"
-      "e8yu33l192GMf6oyTI3QX7I2D4TdWWLTSNfBbxIs9jpaUMpkBmz01NBjsKFOsaz5X09wHH3C"
-      "gfT3BRMiDd3Qi_iDBaNSZ5pz_ij3TD_nLIsfGikxCpPlIj8KPAIffrWczl4X5ikNh8Bj-"
-      "X67YbDKZ0iHtEhU18vaGHE6YpBCY_Kl6DBGEJ_fYiN33sP5hjuRJkaNCpHYj_"
-      "Ix6cqJUtZeHSIxez1Q4Jq3KMIVi5Mi5fqpai6hoNcG-mjFbm9jLI1cEh8PQ";
-
   // pubkey must be in PEM format
-  const char *public_key =
-      "-----BEGIN PUBLIC "
-      "KEY-----"
-      "\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1M1vl2mW0ewMctKEoCYG6+"
-      "SgV9TqN+4oIt2ZLkQb1O+yWGTWuu8h2U7yZ+"
-      "Dc1JfSPUd45eU1p9j3rYu5Bs2Labc6zZUYyBMjZXopv/"
-      "AqIOhvuTRRg7v4yRkC6QACniLndPCkanlp/"
-      "8dL98Gmm8x+"
-      "oOjYf1UFbGxjGqqVxfNVZmGi9NLE6AM0e4wmBVknwWTcC3TTHDxgAxHHa0GhL1y7OYsmw9Kz"
-      "1riUWlr0Az3lBclOFACbOp/"
-      "cGnyHnotErw1xKVQtGOv4GIsYQZr4jIeQkoFcqbAQVOk30NjTRNgVra2JzEpMvhbm4l+"
-      "WHK2OfsPfBx6OKTOmet6zJnnNC608jQIDAQAB\n-----END PUBLIC KEY-----";
-  bool valid;
+  char *public_key;
+  size_t lenpubkey;
 
-  printf("\nAvant validation le jeton : %s\n la clée publique : %s\n",
-         parsed_sd_jwt[0], public_key);
+  public_key = get_pub_key(TRUSTED_CERTIFICATE_PATH);
+printf("The public key is : %s\n", public_key);
+  bool valid;
   valid = validate_jwt((const char **)&parsed_sd_jwt[0], &public_key);
   assert(valid == 1);
-  printf("Le jeton est %s.\n", valid ? " validé" : "non validé");
+  printf("Le jeton est %s avec le certificat de confiance : %s.\n", valid ? " validé" : "non validé",TRUSTED_CERTIFICATE_PATH);
 
   char *grants = NULL;
   grants = extract_grant_jwt((const char **)&parsed_sd_jwt[0], "_sd");
@@ -124,7 +91,7 @@ int main() {
 
   // The disclosure need to have the role "EMPLOYEE" as an example
   char *decoded_SD = NULL;
-  decoded_SD = base64_decode((char *)SD_input);
+  decoded_SD = base64_decode((char *)parsed_sd_jwt[1]);
   printf("The decoded SD is %s", decoded_SD);
 
   // The array needs to contain role and EMPLOYEE
@@ -138,14 +105,14 @@ int main() {
   printf("The claim role is %s\n", valid ? "present" : "absent");
   assert(valid == 1);
 
-  valid = is_in_array((const char **)SD_json, nSD_json, "EMPLOYEE");
-  printf("The claim EMPLOYEE is %s\n", valid ? "present" : "absent");
+  valid = is_in_array((const char **)SD_json, nSD_json, "ADMIN1");
+  printf("The claim ADMIN1 is %s\n", valid ? "present" : "absent");
   assert(valid == 1);
 
   PrintArray(SD_json, nSD_json);
 
   char *hash = NULL;
-  assert(SHA256_sum(SD_input, &hash) == 1);
+  assert(SHA256_sum(parsed_sd_jwt[1], &hash) == 1);
   printf("URL-safe Base64 encoded hash: %s\n", hash);
 
   valid = strcmp((const char *)hash, SD_array[1]) == 0;
@@ -154,9 +121,9 @@ int main() {
   printf("Comparaison found to 2nd sd signed hash %s \n",
          valid ? "same, the disclosure is valid"
                : "differs disclosure isn't valid");
-  valid = check_claim_validity((const char **)SD_array, nSD_array, SD_input);
+  valid = check_claim_validity((const char **)SD_array, nSD_array, parsed_sd_jwt[1]);
   assert(valid == 1);
-  
+
   // TODO : free() les pointeurs
   return 0;
 }
